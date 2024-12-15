ff0d1bfc49271f98d2eb9b3f5e28cfd65b51728c
Nick <nikitachabanets@gmail.com>
2024-12-16 00:08:34 +0100

    Add game logic and score system

--- rpc.js
+++ rpc.js
@@ -1,5 +1,9 @@
 let computerChoice;
 let userChoice;
+let computerScore = 0;
+let userScore = 0;
+let isDraw = false;
+let hasWon = false; 
 
 function getComputerChoice(){
     let rng = Math.floor(Math.random() * 3);
@@ -16,12 +20,46 @@ function getComputerChoice(){
 function getUserChoice(){
     userChoice = prompt("Type in 'Rock', 'Paper' or 'Scissors'", "").toLowerCase();
 
-    if(userChoice !== "rock" || userChoice !== "paper" || userChoice !== "scissors"){
-        userChoice = "scissors";  // for some users it's really hard to type in "scissors"
+    switch(userChoice){
+        case "rock":
+        case "paper":
+        case "scissors": break;
+        default: userChoice = "scissors"; break; // for some users it's really hard to type in "scissors"
     }
 
     console.log(userChoice);
 }
 
+function decideWinner(){
+    if(userChoice === computerChoice){
+        isDraw = true;
+    }else if(userChoice === "rock" && computerChoice === "scissors"){
+        hasWon = true;
+    }else if(userChoice === "paper" && computerChoice === "rock"){
+        hasWon = true;
+    }else if(userChoice === "scissors" && computerChoice === "paper"){
+        hasWon = true;
+    }
+
+    increaseScore();
+}
+
+function increaseScore(){
+    if(isDraw){
+        console.log(`Draw. You both used ${userChoice}`);
+        computerScore ++;
+        userScore ++;
+    }else if(hasWon){
+        console.log(`You won! By ${userChoice} and ${computerChoice}`);
+        userScore ++;
+    }else{
+        console.log(`You lose. By ${userChoice} and ${computerChoice}`);
+        computerScore ++;
+    }
+}
+
 getComputerChoice();
 getUserChoice();
+decideWinner();
+
+
