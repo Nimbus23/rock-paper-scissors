6cf37ec74dee5bf3f162835804c5b4e5c8a887c2
Nick <nikitachabanets@gmail.com>
2024-12-15 23:26:57 +0100

    Add function to get computer's choice

--- rpc.js
+++ rpc.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+let computerChoice;
+
+function getComputerChoice(){
+    let rng = Math.floor(Math.random() * 3);
+
+    switch(rng){
+        case 0: computerChoice = "rock"; break;
+        case 1: computerChoice = "paper"; break;
+        default: computerChoice = "scissors"; break;
+    }
+
+    console.log(computerChoice);
+}
+
+getComputerChoice();
